1f81684dff26f9241c16f5a74560690c6a4f9237
Joash nyabuti <nyabutijoash@yahoo.com>
2021-02-18 12:19:26 -0600

    added components folder with custommer component

---
+++ src/components/customer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const customer = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+
+export default customer
---
+++ src/components/customerList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import customer from './customer'
+
+
+const customerList = () => {
+    return (
+        <div>
+           <customer/> 
+        </div>
+    )
+}
+
+export default customerList
